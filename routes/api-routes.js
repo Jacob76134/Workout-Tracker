1231f921e28517897d3bb6df859b8bb6645e3594
Jacob Brewer <jacobbrewerrooney@gmail.com>
2022-02-02 13:08:50 -0500

    routes and database connected

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,52 @@
 const router = require('express').Router();
-const Workout = require('../models/Workout.js');
+const Workout = require('../models/Workout');
 
 
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+});
+
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(
+        params.id,
+        { $push: { exercises: body } },
+        { new: true }
+
+    ).then(dbWorkout => {
+        res.json(dbWorkout);
+
+    }).catch(err => {
+        res.json(err);
+
+    });
+});
+
+
+router.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.get("/api/workouts/range", function (req, res) {
+    Workout.find({}).limit(5)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,23 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const PORT = 3000
-const mongoose = require('mongoose')
-const db = require('./models')
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3000;
+const mongoose = require('mongoose');
+const apiRoutes = require("./routes/api-routes");
+const viewRoutes = require("./routes/view-routes")
+
+const dotenv = require('dotenv')
+dotenv.config()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+app.use(apiRoutes);
+app.use(viewRoutes)
 
-mongoose.connect(process.env.MONGODB_CONNECT || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
+{
     useNewUrlParser: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
@@ -18,7 +25,7 @@ mongoose.connect(process.env.MONGODB_CONNECT || "mongodb://localhost/workout", {
 });
 
 app.listen(PORT, () => {
-    console.log('server running...')
-})
+    console.log('server running...');
+});
 
-module.exports = mongoose
+module.exports = mongoose;
